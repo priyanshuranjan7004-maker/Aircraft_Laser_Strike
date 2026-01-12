8aaacb64b64a0367fa19bda9d8f622209cc55158
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-12 20:21:56 +0530

    do it

--- obstacles.py
+++ obstacles.py
@@ -6,6 +6,7 @@ game = True
 
 class Obstacles:
     def __init__(self,turt_obst):
+        time.sleep(3)
         self.turt_obst =turtle.Turtle()
         self.turt_obst.shape("square")
         self.turt_obst.shapesize(stretch_wid=2,stretch_len=2)
