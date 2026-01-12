ccb277df4fd5b9af5a84b056388052070ba180c3
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-12 20:22:13 +0530

    Still, Upgrading it

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+import random
 import turtle
 from obstacles import Obstacles
+
+
+
+op = 5
 
 screen = turtle.Screen()
 screen.setup(width=600,height=400)
 
-obs_s = turtle.Turtle()
+def obstacle():
+    enemy =Obstacles()
+    return enemy
 
-enemy =Obstacles(obs_s)
 
-# enemy.
+
+square_obstacles = obstacle()
+Final_enemy =square_obstacles.turt_obst
 
 t = turtle.Turtle()
 t.shape("turtle")
 t.penup()
 
 t.goto(0,-200)
-t.pendown()
+# t.pendown()
 
 t.shapesize(stretch_len=2,stretch_wid=2)
 def move_forward():
@@ -26,5 +34,14 @@ def turn_left():
 screen.onkey(move_forward,"Up")
 screen.onkey(turn_left,"Left")
 
-screen.listen()
-screen.mainloop()
+while op < 10:
+    op+=1
+    print(op)
+    x_axis = random.randint(20,60)
+    y_axis = random.randint(300,400)
+    position_vector = turtle.Vec2D(x_axis, y_axis)
+    print(position_vector)
+    print("Okey")
+    Final_enemy.goto(position_vector)
+    screen.listen()
+    screen.mainloop()
