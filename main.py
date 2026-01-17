d217a6d9380b9192a53316f3f17be87234415449
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-17 19:52:27 +0530

    Still, Upgrading it

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import time
-from turtle import Screen
+import turtle
+# from turtle import Screen
 from obstacles import Obstacles
 from player import Player
-from Bullets import Bullet
-
-screen = Screen()
-screen.setup(width=600,height=600)
-screen.tracer(0)
 
 
 player = Player()
 obstacles =Obstacles()
-Bullets = Bullet()
+lasers = []
+
+
+def bullets():
+   bull= turtle.Turtle()
+   bull.penup()
+   bull.color(1,0,0)
+   bull.setposition(player.xcor(),player.ycor())
+   bull.setheading(90)
+   bull.forward(20)
+   bull.pendown()
+   bull.pensize(5)
+   lasers.append(bull)
+
+def move_lasers(lasers):
+    lasers.clear()
+    lasers.forward(10)
+    lasers.forward(10)
+
+
+screen = turtle.Screen()
+screen.setup(width=600,height=600)
+screen.tracer(0)
 
 
 # bull = Bullets.create_bullets(self=)a
 screen.listen()
 screen.onkey(player.go_up, "Up")
 screen.onkey(player.go_left, "Left")
-screen.onkey(player.go_right, "Right")
-screen.onkey(Bullets.create_bullets,"A")
+screen.onkey(bullets,"space")
+
+
 
 
 game_is_on = True
@@ -28,6 +47,17 @@ while game_is_on:
     screen.update()
     obstacles.create_obstacles()
     obstacles.move_obstacles()
-    Bullets.move_forward()
+    for laser in lasers:
+        move_lasers(laser)
+    # for laser in lasers and for obs in obstacles:
+    #     if laser.po
+
+    print(lasers)
+    # print(obstacles.xcor())
+
+
+
+    print(player.pos())
+
 
 screen.exitonclick()
