ca47d032ce66e77df5c405b4ae20e6800ead5452
Priyanshu R <priyanshuranjan7004@GMAIL.COM>
2026-01-19 19:48:45 +0530

    Still, Upgrading it

--- Scoreboard.py
+++ Scoreboard.py
@@ -2,10 +2,17 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
+        self.score = 0
         super().__init__()
-        self.speed("fastest")
+        # self.speed("fastest")
         self.penup()
         self.hideturtle()
         self.goto(-295,300)
-        self.color("blue")
+        self.color(1,1,1)
         font_style = ("Arial",24,"bold")
+        print("hello")
+
+        self.write("score:0",font = ("Arial",24,"bold"))
+    def update (self,sel):
+        self.clear()
+        self.write(f"score:{sel}")
--- main.py
+++ main.py
@@ -3,70 +3,77 @@ import turtle
 import math
 from obstacles import Obstacles
 from player import Player
+from Scoreboard import Scoreboard
 
+Hit_Score = 0
 
 player = Player()
 obstacles =Obstacles()
 lasers = []
 
+#
+# def bullets():
+#    bull= turtle.Turtle()
+#    bull.penup()
+#    bull.shapesize(stretch_wid=1,stretch_len=1)
+#    bull.color(1,0,0)
+#    bull.setposition(player.xcor(),player.ycor())
+#    bull.setheading(90)
+#    bull.forward(20)
+#    # bull.pendown()
+#    bull.pensize(5)
+#    lasers.append(bull)
+#
+# def move_lasers(las):
+#     las.forward(10)
+#     las.forward(10)
 
-def bullets():
-   bull= turtle.Turtle()
-   bull.penup()
-   bull.shapesize(stretch_wid=1,stretch_len=1)
-   bull.color(1,0,0)
-   bull.setposition(player.xcor(),player.ycor())
-   bull.setheading(90)
-   bull.forward(20)
-   # bull.pendown()
-   bull.pensize(5)
-   lasers.append(bull)
-
-def move_lasers(las):
-    las.forward(10)
-    las.forward(10)
-
-
-def destroy_obstacles(la, ob):
-    for lase, obstacle in zip(la, ob):
-        print(f"lase.xcor ={math.floor(lase.ycor())}")
-        print(f"obstacle.xcor ={math.floor(obstacle.ycor())}")
-    for obs in ob :
-        for lase in la :
-            if obs.ycor() in range(math.floor(lase.ycor())-20,math.floor(lase.ycor())+20):
-                print("Y:Sucess")
-                if obs.xcor() in range(math.floor(lase.xcor())-20, math.floor(lase.xcor())+20):
-                    print("X:Sucess")
-                    lase.hideturtle()
-                    lase.color("black")
-                    lase.setposition(310,310)
-                    ob.remove(obs)
-                    obs.setposition(-310,-310)
-                    la.remove(lase)
+#
+# def destroy_obstacles(la, ob):
+#     for lase, obstacle in zip(la, ob):
+#         print(f"lase.xcor ={math.floor(lase.ycor())}")
+#         print(f"obstacle.xcor ={math.floor(obstacle.ycor())}")
+#     for obs in ob :
+#         for lase in la :
+#             if obs.ycor() in range(math.floor(lase.ycor())-20,math.floor(lase.ycor())+20):
+#                 print("Y:Sucess")
+#                 if obs.xcor() in range(math.floor(lase.xcor())-20, math.floor(lase.xcor())+20):
+#                     print("X:Sucess")
+#                     lase.hideturtle()
+#                     lase.color("black")
+#                     lase.setposition(310,310)
+#                     ob.remove(obs)
+#                     obs.setposition(-310,-310)
+#                     la.remove(lase)
+#                     global Hit_Score
+#                     Hit_Score+=1
+#                     score.update(Hit_Score)
 
 
 screen = turtle.Screen()
 screen.colormode(255)
+score = Scoreboard()
+
 screen.bgcolor(40,40,40)
 screen.title("SPACE INVADERS")
 screen.setup(width=600,height=600)
 screen.tracer(0)
 
 screen.listen()
-screen.onkey(player.go_up, "Up")
-screen.onkey(player.go_left, "Left")
-screen.onkey(bullets,"space")
+# screen.onkey(player.go_up, "Up")
+# screen.onkey(player.go_left, "Left")
+# screen.onkey(bullets,"space")
 
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-    obstacles.create_obstacles()
-    obstacles.move_obstacles()
-    destroy_obstacles(lasers, obstacles.list())
-    for laser in lasers:
-        if laser.ycor() >= 301:
-            lasers.remove(laser)
-        move_lasers(laser)
+# game_is_on = True
+# while game_is_on:
+#     time.sleep(0.1)
+#     screen.update()
+#     obstacles.create_obstacles()
+#     obstacles.move_obstacles()
+#     destroy_obstacles(lasers, obstacles.list())
+#     for laser in lasers:
+#         if laser.ycor() >= 301:
+#             lasers.remove(laser)
+#         move_lasers(laser)
 
 screen.exitonclick()
